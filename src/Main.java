aeae5fc7dd0970690552acb4d0be3b110d9e3793
SMD-JUNAID-BASHA <shaikzzzrahil@gmail.com>
2026-05-21 15:59:13 +0530

    Add Main class for face recognition attendance system

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(" FACE RECOGNITION ATTENDANCE SYSTEM");
+        System.out.println(" FACE RECOGNITION ATTENDANCE SYSTEM ");
+        System.out.println(" FACE RECOGNITION ATTENDANCE SYSTEM");
+
+        // Create Attendance Manager Object
+        AttendanceManager attendance = new AttendanceManager();
+
+        // Mark Attendance
+        attendance.markAttendance(101, "Rahil");
+
+        System.out.println("Attendance Process Completed");
+    }
+}
